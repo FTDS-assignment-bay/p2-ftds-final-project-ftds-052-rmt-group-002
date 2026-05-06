d8fd754283110ce8989b3002ba4e2ef8f00ab868
farhanhl-ds <farhanhamidlubis.ds@gmail.com>
2026-05-06 18:45:31 +0700

    add founder data

---
+++ frontend/src/data/users.ts
@@ -0,0 +1,28 @@
+export const users = [
+  {
+    id: "1",
+    name: "Farhan",
+    username: "FarhanHL",
+    email: "farhan@staywise.ai",
+    avatar: "https://avatars.githubusercontent.com/u/43849669",
+    role: "MLOps",
+  },
+  {
+    id: "2",
+    name: "Zaki",
+    username: "zaki",
+    email: "zaki@staywise.ai",
+    avatar: "",
+    role: "AI Engineer",
+  },
+  {
+    id: "3",
+    name: "Moses",
+    username: "moses",
+    email: "moses@staywise.ai",
+    avatar: "",
+    role: "Data Scientist",
+  },
+];
+
+export const rootUser = users[0];
